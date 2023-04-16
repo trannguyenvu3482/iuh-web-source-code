1e737bdac421f352499b22e54e4c6f5ad274acb8
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-04-16 15:39:52 +0700

    finish UI of product page, working on loading API

--- js/product.js
+++ js/product.js
@@ -1,3 +1,16 @@
+let mainImage = document.querySelector('.main-product__image-main');
+let images = document.querySelectorAll('.main-product__image-preview-item');
+
+let sizes = document.querySelectorAll('.main-product__info-size');
+
+const quantity = document.querySelector('.main-product__info-quantity-input');
+const minusQuantityBtn = document.querySelector(
+  '.main-product__info-quantity-minus'
+);
+const plusQuantityBtn = document.querySelector(
+  '.main-product__info-quantity-plus'
+);
+
 // Get query string from url
 const getQueryString = () => {
   var result = {},
@@ -28,4 +41,68 @@ const getProduct = async () => {
 
 getProduct().then((data) => {
   console.log(data);
+});
+
+// Zoom image handler
+mainImage.addEventListener('mousemove', (e) => {
+  // Get mouse position, width and height of image
+  const { offsetX: x, offsetY: y } = e;
+  const { offsetWidth: width, offsetHeight: height } = mainImage;
+
+  // Calculate background position, without going out of bounds
+  const bgX = Math.min(Math.max(0, (x / width) * 100), 100);
+  const bgY = Math.min(Math.max(0, (y / height) * 100), 100);
+
+  // Set background position
+  mainImage.style.backgroundPosition = `${bgX}% ${bgY}%`;
+  mainImage.style.backgroundSize = '150%';
+});
+
+mainImage.addEventListener('mouseleave', () => {
+  mainImage.style.backgroundPosition = 'center';
+  mainImage.style.backgroundSize = 'cover';
+});
+
+// Change image handler
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    // Remove active class from current image
+    document
+      .querySelector('.main-product__image-preview-item.active')
+      .classList.remove('active');
+
+    // Add active class to parent .main-product__image-preview-item element
+    e.target.parentElement.classList.add('active');
+
+    // Update current image
+    currentImage = e.target;
+
+    // Update main image
+    mainImage.style.backgroundImage = `url(${e.target.src})`;
+  });
+});
+
+// Increase quantity handler
+plusQuantityBtn.addEventListener('click', () => {
+  quantity.value = parseInt(quantity.value) + 1;
+});
+
+// Decrease quantity handler
+minusQuantityBtn.addEventListener('click', () => {
+  if (quantity.value > 1) {
+    quantity.value = parseInt(quantity.value) - 1;
+  }
+});
+
+// Handle size selection
+sizes.forEach((size) => {
+  size.addEventListener('click', (e) => {
+    // Remove active class from current size
+    document
+      .querySelector('.main-product__info-size.active')
+      .classList.remove('active');
+
+    // Add active class to current size
+    e.target.classList.add('active');
+  });
 });
